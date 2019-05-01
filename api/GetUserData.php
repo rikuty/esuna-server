b213073e9dad1cf606be589aaa0d014538f77a4a
LunaSun <kawano@luna-sun.com>
2019-05-01 19:40:24 +0900

    修正

--- api/GetUserData.php
+++ api/GetUserData.php
@@ -63,7 +63,7 @@ try {
 */
 require './../common/conf.php';
 
-$stmt = $pdo->query("SELECT user_id, CONCAT(last_name, first_name) as name FROM u_user");
+$stmt = $pdo->query("SELECT user_id, CONCAT(last_name, ' ', first_name) as name FROM u_user");
 while($row = $stmt -> fetch(PDO::FETCH_ASSOC)) {
     echo $row["user_id"]." : ".$row["name"]."\n";
 }
