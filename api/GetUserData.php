8da198a9526e486016cc78860d10a475e24ff9a7
LunaSun <kawano@luna-sun.com>
2019-05-09 14:56:29 +0900

    修正

--- api/GetUserData.php
+++ api/GetUserData.php
@@ -2,10 +2,16 @@
 
 require './../common/conf.php';
 
-$stmt = $pdo->query("SELECT user_id, CONCAT(last_name, ' ', first_name) as user_name FROM u_user");
-while($row = $stmt -> fetch(PDO::FETCH_ASSOC)) {
-    //echo $row["user_id"]." : ".$row["name"]."\n";
-    echo json_encode($row, JSON_UNESCAPED_UNICODE);
+$sql = "SELECT user_id, CONCAT(last_name, ' ', first_name) as user_name FROM u_user WHERE active = 10";
+$stmt = $pdo->query($sql);
+
+if($stmt->rowCount() == 0){
+	// アクティブユーザー該当なし
+	echo 0;
+} else {
+	$row = $stmt -> fetch(PDO::FETCH_ASSOC);
+
+	echo json_encode($row, JSON_UNESCAPED_UNICODE);
 }
 
 ?>
