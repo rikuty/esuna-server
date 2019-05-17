6d68127a15056644f4a70b73eccd0ae5c6b1c18b
LunaSun <kawano@luna-sun.com>
2019-05-17 17:10:56 +0900

    修正

--- api/SetUserData.php
+++ api/SetUserData.php
@@ -2,16 +2,52 @@
 
 require './../common/conf.php';
 
-$user_id 	= $_POST['user_id'];
-$last_name 	= $_POST['last_name'];
-$first_name = $_POST['first_name'];
-$age 	 	= $_POST['age'];
+// 登録内容を連想配列で生成
+$datalist = array(
+	'user_id' => $_POST['user_id'], 
+	'measure_date' => date("Y-m-d H:i:s"), 
+	'max_rom_measure_1' => $_POST['max_rom_measure_1'],
+	'max_rom_measure_2' => $_POST['max_rom_measure_2'],
+	'max_rom_measure_3' => $_POST['max_rom_measure_3'],
+	'max_rom_measure_4' => $_POST['max_rom_measure_4'],
+	'max_rom_measure_5' => $_POST['max_rom_measure_5'],
+	'max_rom_measure_6' => $_POST['max_rom_measure_6'],
+	'max_rom_measure_7' => $_POST['max_rom_measure_7'],
+	'max_rom_measure_8' => $_POST['max_rom_measure_8'],
+	'pre_rest_pain' => $_POST['pre_rest_pain'], 
+	'pre_move_pain' => $_POST['pre_move_pain'], 
+	'pre_move_fear' => $_POST['pre_move_fear'], 
+	);
 
-$sql = "UPDATE u_user SET ".
-		"last_name = '".$last_name."', ".
-		"first_name = '".$first_name."', ".
-		"age = ".$age." ".
-	   "WHERE user_id = ".$user_id;
+// 新規レコード追加(+ 診断データ更新)
+
+$sql = "INSERT INTO u_measure (";
+
+$count = 0;
+foreach ( $datalist as $key => $value ) {
+    $sql += $key;
+
+    $count++;
+    if($count == count($datalist)){
+    	$sql += ") ";
+    }else{
+    	$sql += ", ";
+    }
+}
+
+$sql += "VALUES (";
+
+$count = 0;
+foreach ( $datalist as $key => $value ) {
+    $sql += $value;
+
+    $count++;
+    if($count == count($datalist)){
+    	$sql += ") ";
+    }else{
+    	$sql += ", ";
+    }
+}
 
 $stmt = $pdo->query($sql);
 $stmt->execute();
