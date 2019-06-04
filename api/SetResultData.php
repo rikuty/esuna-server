640b4b896aa5998100ddc851d7a676035c29958f
LunaSun <kawano@luna-sun.com>
2019-06-04 16:51:35 +0900

    修正

--- api/SetResultData.php
+++ api/SetResultData.php
@@ -2,13 +2,14 @@
 
 require './../common/conf.php';
 
+error_log("SetResult php");
 // 画像形式確認
 if ($_FILES["file"]["type"] == "image/png") {
 
 	$uploadFile = "/var/www/user_result/".$_POST['user_id']."/".$_FILES["file"]["name"];
 
     if ($_FILES["file"]["error"] > 0) {
-        echo "Return Code:" . $_FILES["file"]["error"] . "";
+        error_log("Return Code:" . $_FILES["file"]["error"]);
         Return;
     } else {
         //echo "Upload:" . $_FILES["file"]["name"] . "";
