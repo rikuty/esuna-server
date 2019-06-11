c27857fa53a15526e2d7c270666a4c9106713b24
LunaSun <kawano@luna-sun.com>
2019-06-11 16:22:45 +0900

    修正

--- approve.php
+++ approve.php
@@ -14,19 +14,19 @@ $content = http_build_query($query, '', '&');
  
 //ヘッダ設定
 $header = array(
-	'Content-Type: application/x-www-form-urlencoded', //form送信の際、クライアントがWebサーバーに送信するコンテンツタイプ
+	'Authorization: OAuth ya29.GlslB38_yAcf3SFDbIgOZesXXquwNvUmndyGqd49lOchgOvO2mul4CB5vJBNzsTPC9OhQ7bTyoPp2AHc6UQD_XIYwep6CEOlPRgmgXWM8f5Pr-0TAoaSoCBT3-G_', //form送信の際、クライアントがWebサーバーに送信するコンテンツタイプ
 );
  
 //ストリームコンテキスト設定
 $context = array(
 	'http' => array(
 		'ignore_errors' => true, //ステータスコードが失敗を意味する場合でもコンテンツを取得
-		'method' => 'POST', //メソッド。デフォルトはGET
+		'method' => 'GET', //メソッド。デフォルトはGET
 		'header' => implode("\r\n", $header), //ヘッダ設定
-		'content' => $content //送信したいデータ
+		//'content' => $content //送信したいデータ
 	)
 );
-$url = 'https://www.googleapis.com/oauth2/v4/token';
+$url = 'https://www.google.com/cloudprint/search';
 $res = file_get_contents($url, false, stream_context_create($context));
 
 echo('<pre>');
