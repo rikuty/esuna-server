d4329a8d7d9ad211a0d47782507faf5a64473f99
LunaSun <kawano@luna-sun.com>
2019-06-11 16:37:29 +0900

    修正

---
+++ print_submit.php
@@ -0,0 +1,35 @@
+<?php
+
+//POSTで送りたいデータ
+$query = array(
+	'printerId' => 'cb69ed03-b40b-7f6c-1c21-aef97985cfd6', 
+	'title' => 'test', 
+	'contentType' => 'url',
+	'content' => 'LICENSE.txt',
+	'ticket' => '{"version":"1.0","print":{"vendor_ticket_item":[],"color":{"type":"STANDARD_MONOCHROME"},"copies":{"copies":1}}}'
+);
+ 
+//URLエンコードされたクエリ文字列を生成
+$content = http_build_query($query, '', '&');
+
+
+//ヘッダ設定
+$header = array(
+	'Content-Type: application/x-www-form-urlencoded', //form送信の際、クライアントがWebサーバーに送信するコンテンツタイプ
+);
+ 
+//ストリームコンテキスト設定
+$context = array(
+	'http' => array(
+		'ignore_errors' => true, //ステータスコードが失敗を意味する場合でもコンテンツを取得
+		'method' => 'POST', //メソッド。デフォルトはGET
+		'header' => implode("\r\n", $header), //ヘッダ設定
+		'content' => $content //送信したいデータ
+	)
+);
+$url = 'https://www.google.com/cloudprint/submit';
+$res = file_get_contents($url, false, stream_context_create($context));
+
+var_dump($res);
+
+?>
