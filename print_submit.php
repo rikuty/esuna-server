728d08bb4abdb5dc5a21ff08a94b1d738f39d9a3
LunaSun <kawano@luna-sun.com>
2019-06-11 17:01:39 +0900

    修正

--- print_submit.php
+++ print_submit.php
@@ -18,7 +18,7 @@ $content = http_build_query($query, '', '&');
 //ヘッダ設定
 $header = array(
 	'Content-Type: application/x-www-form-urlencoded', //form送信の際、クライアントがWebサーバーに送信するコンテンツタイプ
-	'Content-Type: image/png'
+	//'Content-Type: image/png'
 );
  
 //ストリームコンテキスト設定
