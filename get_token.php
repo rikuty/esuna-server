ead6d08bc533214d4906ba34189f6ef4c7c3ee78
LunaSun <kawano@luna-sun.com>
2019-06-13 16:12:41 +0900

    修正

--- get_token.php
+++ get_token.php
@@ -1,5 +1,38 @@
 <?php
 
+$url = 'https://www.googleapis.com/oauth2/v4/token';
+$data = [];
+
+// $dataに送るデータを詰めます。
+$data['code'] = '4/aAHP-3C50FXG2TdSyC-RsxSr_K9nIkFmjpxnwg3SQDfANi8-McnqxzY';
+$data['client_id'] = '761889929248-hec2iu2qn5ae35qefm1ji0htu0lmhjmd.apps.googleusercontent.com';
+$data['client_secret'] = 'F3Zva_8sFjFUiuHtjWnD-8Cc';
+$data['redirect_uri'] = 'urn:ietf:wg:oauth:2.0:oob';
+$data['grant_type'] = 'authorization_code';
+$data['access_type'] = 'offline';
+
+// 送信データをURLエンコード
+$data = http_build_query($data, "", "&");
+
+// curlを初期化
+$ch = curl_init();
+
+// 設定!
+curl_setopt($ch, CURLOPT_URL, $url); // 送り先
+curl_setopt($ch, CURLOPT_POST, true); // POSTです
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data); // 送信データ
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // 実行結果取得の設定
+
+// 実行！
+$res = curl_exec($ch);
+
+var_dump($res);
+
+// リソースを閉じる
+curl_close($ch);
+
+
+/*
 //POSTで送りたいデータ
 $query = array(
 	'code' => '4/aAHP-3C50FXG2TdSyC-RsxSr_K9nIkFmjpxnwg3SQDfANi8-McnqxzY',
@@ -33,5 +66,5 @@ $res = file_get_contents($url, false, stream_context_create($context));
 echo('<pre>');
 var_dump($res);
 echo('</pre>');
-
+*/
 ?>
