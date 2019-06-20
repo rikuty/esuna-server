4ed4113b35f10f46dffa8589020438e537a3f225
LunaSun <kawano@luna-sun.com>
2019-06-20 19:03:59 +0900

    修正

--- image.php
+++ image.php
@@ -1,5 +1,8 @@
 <?php
 
+if(!array_key_exists("user_id", $_GET)){
+        exit('user_idを指定してください');
+}
 $user_id = $_GET["user_id"];
 
 $image_path = '/var/www/user_result/'.$user_id.'/ResultSheet.png';
