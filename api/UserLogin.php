29b899a837e61cddeb4dfcbf32956ad4ec51b4ff
LunaSun <kawano@luna-sun.com>
2019-08-25 14:30:57 +0900

    パスワード廃止

--- api/UserLogin.php
+++ api/UserLogin.php
@@ -3,9 +3,8 @@
 require './../common/conf.php';
 
 $userId = $_POST['user_id'];
-$password = $_POST['password'];
 
-$sql = "SELECT user_id, CONCAT(last_name, ' ', first_name) as user_name FROM u_user WHERE user_id = ".$userId." AND password = ".$password;
+$sql = "SELECT user_id, CONCAT(last_name, ' ', first_name) as user_name FROM u_user WHERE user_id = ".$userId;
 $stmt = $pdo->query($sql);
 
 if($stmt->rowCount() == 0){
@@ -16,5 +15,4 @@ if($stmt->rowCount() == 0){
 
 	echo json_encode($row, JSON_UNESCAPED_UNICODE);
 }
-
 ?>
