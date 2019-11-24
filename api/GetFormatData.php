3ff08b4623ba83bf5dd12cafec63c17b11b9e755
LunaSun <kawano@luna-sun.com>
2019-11-24 15:16:15 +0900

    GetFormatData修正

--- api/GetFormatData.php
+++ api/GetFormatData.php
@@ -2,28 +2,12 @@
 
 require './../common/conf.php';
 
-$sql = "SELECT user_id, CONCAT(last_name, ' ', first_name) as user_name, age, height, rank FROM u_user WHERE active = 10";
+$resultData = $stmt -> fetch(PDO::FETCH_ASSOC);
+
+$sql = "SELECT * FROM u_measure WHERE user_id = ".$_POST['user_id']." ORDER BY measure_date DESC LIMIT 20";
 $stmt = $pdo->query($sql);
 
-if($stmt->rowCount() == 0){
-	// アクティブユーザー該当なし
-	echo 0;
-} else {
-	$resultData = $stmt -> fetch(PDO::FETCH_ASSOC);
-
-	$sql = "SELECT * FROM u_measure WHERE user_id = ".$resultData['user_id']." ORDER BY measure_date DESC LIMIT 10";
-	$stmt = $pdo->query($sql);
-
-	$measureData = array();
-	$index = 1;
-	while($row = $stmt -> fetch(PDO::FETCH_ASSOC)) {
-		$measureData[$index] = $row;
-		$index++;
-	}
-	$resultData["measure"] = $measureData;
-
-	//var_dump($resultData);
-	echo json_encode($resultData, JSON_UNESCAPED_UNICODE);
-}
+$row = $stmt -> fetch(PDO::FETCH_ASSOC);
+echo json_encode($row, JSON_UNESCAPED_UNICODE);
 
 ?>
